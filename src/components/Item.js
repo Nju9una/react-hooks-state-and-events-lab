0bd9a627a7770b79ca979ba18bcc27a6c343ab9b
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-05-01 02:57:13 +0300

    state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ name, category }) {
+function Item({ id, name, category }) {
+  
+  const [isInCart, setIsInCart] = useState(false);
+
+ 
+  const toggleCartStatus = () => {
+    setIsInCart(!isInCart);
+  };
+
+ 
+  const liClass = isInCart ? "in-cart" : "";
+
+  
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Button to add/remove item from cart */}
+      <button className="add" onClick={toggleCartStatus}>
+        {buttonText}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredItems = selectedCategory === 'All' ? items : items.filter(item => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +21,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {filteredItems.length === 0 ? (
+          <li>No items found</li>
+        ) : (
+          filteredItems.map((item) => (
+            <Item key={item.id} id={item.id} name={item.name} category={item.category} />
+          ))
+        )}
       </ul>
     </div>
   );
